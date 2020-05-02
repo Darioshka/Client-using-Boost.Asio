268d4d6894b8ba8488b07264fdd2a83ee36e9f65
Darioshka <56868583+Darioshka@users.noreply.github.com>
2020-05-02 03:05:59 +0300

    5

--- include/header.hpp
+++ include/header.hpp
@@ -6,17 +6,23 @@
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 #include <iostream>
-#include <stdio.h>
 #include <ctime>
 #include <string>
 
 #define PORT_NUM 8001
 #define MS 100
+#define SEC 7
 
 const char STR[] = "127.0.0.1";
 const char CONNECTED[] = "client connected ";
 const char LIST_CHANED[] = "client list chaned\n";
 const char PING[] = "ping \n";
+const char PING_OK[] = "ping";
+const char LOGIN[] = "login";
+const char CLIENT[] = "client";
+const char LIST[] = "List";
+const char CLIENTS[] = "clients \n";
+
 
 using std::thread;
 using std::exception;
@@ -33,88 +39,17 @@ private:
     std::string username_;
 
 public:
-    explicit talk_to_server(const std::string &username) :
-                            sock_(context), username_(username) {}
-    sock &my_socket() { return sock_; }
-    std::string get_username() {
-        return username_;
-    }
-
-    void connect(endpoint ep) {
-        sock_.connect(ep);
-        std::cout << CONNECTED << std::endl;
-    }
-
-    void write(const std::string &msg) {
-        boost::asio::streambuf buffer{};
-        std::ostream out(&buffer);
-        out << msg;
-        boost::asio::write(my_socket(), buffer);
-    }
-
-    void read() {
-        boost::asio::streambuf buffer{};
-        boost::asio::read_until(my_socket(), buffer, '\n');
-
-        std::string answer(std::istreambuf_iterator<char>{&buffer},
-                           std::istreambuf_iterator<char>{});
-
-        process_request(answer);
-    }
-
-    void on_login(const std::string msg) {
-        std::cout << msg << std::endl;
-    }
-
-    void on_ping(const std::string &msg) {
-        if (msg == LIST_CHANED){
-            write(PING);
-            read();
-        }
-            std::cout << msg << std::endl;
-    }
-
-
-    void on_client(const std::string &msg) {
-        std::cout << msg << std::endl;
-    }
-
-    void process_request(std::string answ) {
-        if (answ.find("ping") == 0) on_ping(answ);
-        if (answ.find("login") == 0) on_login(answ);
-        if (answ.find("client") == 0) on_ping(answ);
-        if (answ.find("For") == 0) on_client(answ);
-    }
-
-    void action() {
-        write("login " + username_ + "\n");
-        read();
-        while (true) {
-            write("ping \n");
-            read();
-            write("clients \n");
-            read();
-            unsigned r_time = time(nullptr);
-            sleep(1 + rand_r(&r_time) % 7);
-        }
-    }
+    explicit talk_to_server(const std::string &username);
+    sock &my_socket();
+    std::string get_username();
+    void connect(endpoint ep);
+    void write(const std::string &msg);
+    void read();
+    void on_login(const std::string msg);
+    void on_ping(const std::string &msg);
+    void on_client(const std::string &msg);
+    void process_request(std::string answ);
+    void action() ;
 };
-
-void run_client(const std::string & client_name){
-    endpoint ep(input_ip::from_string(STR), PORT_NUM);
-    talk_to_server client(client_name);
-    client.connect(ep);
-    client.action();
-}
-
-int main() {
-    std::string client_name;
-    std::cin >> client_name;
-    client_name = client_name.substr(0, client_name.length());
-    std::thread th(boost::bind(run_client, client_name));
-    boost::this_thread::sleep(boost::posix_time::millisec(MS));
-    th.join();
-    return 0;
-}
 
 #endif // INCLUDE_HEADER_HPP_
